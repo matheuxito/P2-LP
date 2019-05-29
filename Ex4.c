5d6957e3085bb3c075e4909a7f306ad6bd2a84ef
Matheus Silva <51101359+matheuxito@users.noreply.github.com>
2019-05-28 22:34:31 -0300

    Exercício 4 - Palíndromo

---
+++ Ex4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>  // Biblioteca para strings
+#define MAX 2000  
+
+int main(void){
+  char palavra[MAX]; 
+  int i, j, N; 
+  printf("Escreva uma Palavra: ");
+  scanf("%s",palavra);   
+  N = strlen(palavra);   // strlen retorna o tamanho da palavra
+  j = N-1;
+  for (i=0; i<N; i++){
+    if (palavra[i]==palavra[j])  // Analisando se a palavra é igual ao inverso dela        
+      j--;                      
+    else{    // Caso em algum ponto ela não ser igual ao inverso dela, ela não será um palíndromo
+      printf("\n%s não é um Palíndromo",palavra);    
+      return 0;
+    }
+  }
+  printf("\n%s é um Palíndromo",palavra); // Como a palavra é igual ao inverso dela, ela é palíndromo
+}
